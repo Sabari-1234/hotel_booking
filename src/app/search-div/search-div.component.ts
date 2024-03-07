e26b17cf23650f862c7c25b2685c1d9c66fc8a29
Sabari-1234 <sabarinathan5802@gmail.com>
2024-03-07 13:13:16 +0530

    search and filter uniqueness

--- src/app/search-div/search-div.component.ts
+++ src/app/search-div/search-div.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../shared/api.service';
 
 @Component({
   selector: 'app-search-div',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './search-div.component.css',
 })
 export class SearchDivComponent {
-  fun() {}
+  constructor(public api: ApiService) {}
+  fun(e: any) {
+    console.log(e.target.value);
+    var regex = new RegExp(e.target.value, 'i');
+    this.api.getBlock().subscribe(
+      (res) => {
+        this.api.details = res;
+        console.log(regex);
+        console.log(this.api.details);
+        this.api.details = this.api.details.filter((res: any) =>
+          regex.test(res.city)
+        );
+        console.log(this.api.details);
+      },
+      (err) => console.log(err)
+    );
+  }
+  closed(search: any) {
+    console.log(search);
+    console.log(search.value);
+    search.value = '';
+    this.api.getBlock().subscribe(
+      (res) => {
+        this.api.details = res;
+
+        console.log(this.api.details);
+      },
+      (err) => console.log(err)
+    );
+  }
 }
